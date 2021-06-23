4e3079b838e32473b10e10fee909bf32bb66123b
Sakshi Kadam <sakshikadam1520@gmail.com>
2021-06-23 20:59:54 +0530

    test

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // require('dotenv').config()
 const bodyParser = require('body-parser')
 const express = require('express')
-// const cors = require('cors');
 const app = express()
-// app.use(cors());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+const cors = require('cors');
+
+app.use(cors({origin: "*"}));
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
 
 const mongoose = require('mongoose')
 
