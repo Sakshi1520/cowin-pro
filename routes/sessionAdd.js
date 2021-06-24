3e742566853b1db892ff37fe9cce9523ebfc2960
Sakshi Kadam <sakshikadam1520@gmail.com>
2021-06-24 13:01:56 +0530

    allSessions API

--- routes/sessionAdd.js
+++ routes/sessionAdd.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const Vaccinator = require('../model/vaccinatorSchema')
 const Session = require('../model/sessionSchema')
 
 // session instance
@@ -27,6 +28,32 @@ router.post('/addSession', async(req,res) => {
     })
 })
 
+router.get('/allSessions', async(req,res) => {
+    try{
+        let session = []
+        const vaccinator = await Vaccinator.find()
+        if(vaccinator.length === 0){
+            res.send('No Vaccinators found.')
+        }
+        else{
+            let promises =  vaccinator.map(async(vaccinator)=>{
+                let session =await  Session.find()
+                let sessions = session[0];
+                let app = vaccinator._doc;
+                vaccinator = {...app, sessions}
+                return vaccinator;
+            });
+        
+            centers = await Promise.all(promises);
+            res.send({centers})
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.send.status(500).send({message: err.message})
+    }
+})
+
 // router.get('/:id', async(req,res) => {
 //     var session = await Session.findById(req.params.id);
 //     res.send(session)
@@ -43,10 +70,14 @@ router.get('/:id', async(req,res) => {
     }
 });
 
+
+
 //get session by centerID
 router.get('/', async(req,res) => {
     var sessions = await Session.find().where('centerId').equals(req.query.centerId);
     res.send(sessions)
 });
+
+
 
 module.exports = router
